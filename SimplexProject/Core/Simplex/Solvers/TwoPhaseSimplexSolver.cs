8cd866eba067217e121becaa9cecab650fbbcaa2
Dugd_Nitkin <dugd.nitkin@gmail.com>
2024-10-25 19:22:35 +0300

    add Simplex classes

---
+++ SimplexProject/Core/Simplex/Solvers/TwoPhaseSimplexSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplexProject.Core.Simplex.Solvers
+{
+    internal class TwoPhaseSimplexSolver : ISimplexSolver
+    {
+        public SimplexSolve Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
