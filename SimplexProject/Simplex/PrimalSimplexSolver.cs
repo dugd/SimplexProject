732ea051f83bfc6bf4cd20dcfe3c3584fb58db27
Dugd_Nitkin <dugd.nitkin@gmail.com>
2024-10-29 01:47:59 +0200

    write to standart form method

--- SimplexProject/Simplex/PrimalSimplexSolver.cs
+++ SimplexProject/Simplex/PrimalSimplexSolver.cs
@@ -1,17 +1,59 @@
-﻿
+﻿using SimplexProject.Enums;
 
 namespace SimplexProject.Simplex
 {
     internal class PrimalSimplexSolver
     {
+        private LPTask task;
+
         public PrimalSimplexSolver(LPTask task)
         {
-            throw new NotImplementedException();
+            this.task = task;
         }
 
-        public void Init(LPTask task)
+        public static LPTask ConvertToStandartForm(LPTask task)
         {
-            throw new NotImplementedException();
+            int constraintsCount = task.ConstraintsMatrix.GetLength(0);
+            int oldLength = task.ObjectiveFuction.Length;
+            int newVariables = task.RelationTypes.Count(r => r != RelationType.Equal);
+            int newLength = oldLength + newVariables;
+
+            var newConstraintsRHS = new double[constraintsCount];
+            var newConstraintsMatrix = new double[constraintsCount, newLength];
+            RelationType[] newRelationTypes = Enumerable.Repeat(RelationType.Equal, newLength).ToArray();
+
+            int k = oldLength;
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                int sign = task.ConstraintsRHS[i] >= 0 ? 1 : -1;
+                for (int j = 0; j < oldLength; j++)
+                {
+                    newConstraintsMatrix[i, j] = sign * task.ConstraintsMatrix[i, j];
+                }
+
+                if (task.RelationTypes[i] != RelationType.Equal)
+                {
+                    newConstraintsMatrix[i, k] = sign * (task.RelationTypes[i] == RelationType.LessEqual ? 1 : -1);
+                    k++;
+                }
+                newConstraintsRHS[i] = sign * task.ConstraintsRHS[i];
+            }
+
+            var newObjectiveFunction = new double[newLength];
+            Array.Copy(task.ObjectiveFuction, newObjectiveFunction, oldLength);
+
+            return new LPTask(
+                newObjectiveFunction,
+                newConstraintsMatrix,
+                newConstraintsRHS,
+                newRelationTypes,
+                task.Optimization);
+        }
+
+        public void TransformTask()
+        {
+            task = ConvertToStandartForm(task);
+
         }
 
         public void BuildTableau()
@@ -24,9 +66,41 @@ namespace SimplexProject.Simplex
             throw new NotImplementedException();
         }
 
-        public void Solve()
+        public bool IsOptimal()
         {
             throw new NotImplementedException();
+        }
+
+        public void PrintTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintTableau()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintSolution()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Solve()
+        {
+            TransformTask();
+            PrintTask();
+
+            BuildTableau();
+            PrintTableau();
+
+            while (!IsOptimal())
+            {
+                NextIteration();
+                PrintTableau();
+            }
+
+            PrintSolution();
         }
     }
 }
